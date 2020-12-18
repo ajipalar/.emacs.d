7e9bf7ade2009835d4e623c0b2dd97c9c04d817a
Aji <ajikarunia@gmail.com>
2020-12-17 16:11:00 -0800

    using zenburn theme

--- init.el
+++ init.el
@@ -7,7 +7,6 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
 (package-initialize)
 (package-refresh-contents)
-
 ;; Download Evil
 (unless (package-installed-p 'evil)
   (package-install 'evil))
@@ -22,12 +21,34 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   (quote
-    ("2cbd2a0d861fd6baf446f4393f3c5ed00ed861fe9c9073c87a7c8438ada877d4" default))))
+   '("2cbd2a0d861fd6baf446f4393f3c5ed00ed861fe9c9073c87a7c8438ada877d4" default))
+ '(package-selected-packages '(magit evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+;;(load-theme 'monokai)
+(load-theme 'zenburn)
+(setq magit-view-git-manual-method 'man)
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch)
+(global-set-key (kbd "C-c g") 'magit-file-dispatch)
+;; Download julia-mode
+(unless (package-installed-p 'julia-mode)
+  (package-install 'julia-mode))
+;; Enable julia-mode
+(require 'julia-mode)
+
+;;Install julia-shell-mode after julia-mode
+(add-to-list 'load-path "~/package/jlpkgs/julia-shell-mode")
+(require 'julia-shell)
+
+;; Define functions and hooks for julia-shell-mode
+(defun my-julia-mode-hooks()
+  (require 'julia-shell-mode))
+(add-hook 'julia-mode-hook 'my-julia-mode-hooks)
+(define-key julia-mode-map (kbd "C-c C-c") 'julia-shell-run-region-or-line)
+(define-key julia-mode-map (kbd "C-c C-s") 'julia-shell-save-and-go)
 (load-theme 'monokai)
