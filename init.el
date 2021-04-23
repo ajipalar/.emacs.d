86a727abe20f75daa866c4731cf76c593cf570c5
Aji <ajikarunia@gmail.com>
2021-04-23 11:44:50 -0700

    keybindings for vimish folding

--- init.el
+++ init.el
@@ -1,5 +1,9 @@
 ;;A cleaner emacs init-file
 
+;;Themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+;;(load-theme 'zenburn)
+
 ;;Turn on syntax heighlighting no matter the mode
 (global-font-lock-mode 1)
 
@@ -13,3 +17,25 @@
    (c++ . t)
    (awk . t)
    (sed . t)))
+
+;; Download Evil
+(unless (package-installed-p 'evil)
+  (package-install 'evil))
+
+;; Enable Evil
+(require 'evil)
+(evil-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("2cbd2a0d861fd6baf446f4393f3c5ed00ed861fe9c9073c87a7c8438ada877d4" default))
+ '(package-selected-packages '(magit evil)))
+(load-theme 'monokai)
+
+(global-set-key (kbd "C-c C-v") 'vimish-fold)
+(global-set-key (kbd "C-c C-r") 'vimish-fold-refold)       
+(global-set-key (kbd "C-c C-u") 'vimish-fold-unfold)       
